586a2cfc1749bc373b11d3406bbf46df027d6fc2
Eugene Ahn <eugeneahn125@gmail.com>
2022-04-25 15:06:13 -0500

    set up route and controller file structure

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const controller = require("../../controllers/userController");
+
+router.route("/").get(controller.getUser).post(controller.createUser);
+
+router.route("/:userId").get(controller.getSingleUser).put(controller.updateSingleUser).delete(controller.deleteSingleUser);
+
+router.route("/:userId/friends/:friendId").post(controller.addFriend).delete(controller.deleteFriend)
+
+module.exports = router;
