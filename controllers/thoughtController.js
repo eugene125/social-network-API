a946c207c2623d44279f3be3192c0b317e15d382
Eugene Ahn <eugeneahn125@gmail.com>
2022-04-25 15:23:52 -0500

    set up framework for controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,40 @@
+const { ObjectId } = require("mongoose").Types;
+const { User, Thoughts } = require("../models");
+
+const getAllThoughts = () => {
+
+};
+
+const createThought = () => {
+
+};
+
+const getSingleThought = () => {
+
+};
+
+const updateSingleThought = () => {
+
+};
+
+const deleteSingleThought = () => {
+
+};
+
+const createReaction = () => {
+
+};
+
+const getSingleReaction = () => {
+
+};
+
+module.exports = {
+    getAllThoughts,
+    createThought,
+    getSingleThought,
+    updateSingleThought,
+    deleteSingleThought,
+    createReaction,
+    getSingleReaction
+}
---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+const { ObjectId } = require("mongoose").Types;
+const { User, Thoughts } = require("../models");
+
+const getAllUsers = () => {
+    
+};
+
+const createUser = () => {
+
+};
+
+const getSingleUser = () => {
+
+};
+
+const updateSingleUser = () => {
+
+};
+
+const deleteSingleUser = () => {
+
+};
+
+const addFriend = () => {
+
+};
+
+const deleteFriend = () => {
+
+};
+
+module.exports = {
+    getAllUsers,
+    createUser,
+    getSingleUser,
+    updateSingleUser,
+    deleteSingleUser,
+    addFriend,
+    deleteFriend
+}
